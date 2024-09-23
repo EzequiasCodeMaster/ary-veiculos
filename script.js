3d3b6c70c6ec28e21b1288e2eb31576fdb5f91f3
EzequiasCodeMaster <gabrielezequias1524@gmail.com>
2024-09-23 17:11:19 -0300

    imagens se mexendo

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const images = ['imagens/mobi-vermelho.png',
+const images = [
+    'imagens/mobi-vermelho.png',
     'imagens/m-1.jpeg',
     'imagens/m-2.jpeg',
     'imagens/m-3.jpeg',
     'imagens/m-4.jpeg',
     'imagens/m-5.jpeg',
-    'imagens/m-6.jpeg',]; // Substitua com as URLs das imagens do carrossel
+    'imagens/m-6.jpeg'
+]; // Substitua com as URLs das imagens do carrossel
+
 let currentIndex = 0;
 
 function changeImage() {
-    const carouselImg = document.getElementById('carousel-img');
-    currentIndex = (currentIndex + 5) % images.length;
-    carouselImg.src = images[currentIndex];
+    const carouselImages = document.querySelectorAll('.carousel-img'); // Seleciona todas as imagens com a classe 'carousel-img'
+    
+    carouselImages.forEach((carouselImg) => {
+        currentIndex = (currentIndex + 1) % images.length; // Avança o índice da imagem
+        carouselImg.src = images[currentIndex]; // Altera o src da imagem
+    });
 }
 
 // Troca de imagem a cada 1 segundo (1000ms)
